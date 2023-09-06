551dfb292dc8b900ed427e3555f6401d46357472
Yure7S <yuresilvaprof@gmail.com>
2023-09-06 18:32:29 -0300

    Finalizando header e footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { CatalogComponent } from './features/catalog/catalog.component';
+import { AdminComponent } from './features/admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'admin', component: AdminComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HomeComponent } from './features/home/home.component';
+import { CatalogComponent } from './features/catalog/catalog.component';
+import { AdminComponent } from './features/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    CatalogComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
