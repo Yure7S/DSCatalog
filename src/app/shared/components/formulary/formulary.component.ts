6c05ef860b85c67cc3b64fb2024d8c685ea5827d
Yure7S <yuresilvaprof@gmail.com>
2023-09-27 14:30:29 -0300

    Refinando validações

--- src/app/shared/components/formulary/formulary.component.ts
+++ src/app/shared/components/formulary/formulary.component.ts
@@ -19,25 +19,31 @@ export class FormularyComponent {
   
   private createForm() {
     this.loginForm = new FormGroup({
-      email: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [Validators.required])
     });
 
     this.registerForm = new FormGroup({
       name: new FormControl('', [Validators.required]),
       surname: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
       repeatPassword: new FormControl('', [Validators.required])
     });
 
     this.recoverForm = new FormGroup({
-      email: new FormControl('', [Validators.required]),
-      repeatEmail: new FormControl('', [Validators.required])
+      email: new FormControl('', [Validators.required, Validators.email]),
+      repeatEmail: new FormControl('', [Validators.required, Validators.email])
     });
   }
 
-  public onSubmit(value: object) {
+  public onSubmit(form: FormGroup, value: object) {
+
+    if(!form.valid){
+      console.log("O formulário não é válido")
+      return
+    }
+    
     console.log(value)
   }
 }
