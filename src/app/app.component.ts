746a9ce9e0614c02c5b79acd6be00f33898f947a
Yure7S <yuresilvaprof@gmail.com>
2023-10-02 11:57:01 -0300

    Atualizando autenticação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CurrentUserServiceService } from './shared/services/current-user-service.service';
 
 
 @Component({
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DSCatalog';
+  currentUser = inject(CurrentUserServiceService)
+
+  ngOnInit(){
+    this.currentUser.setCurrentUser()
+  }
 }
--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -1,13 +1,13 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Route, Router } from '@angular/router';
+import { CurrentUserServiceService } from 'src/app/shared/services/current-user-service.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
 
   const router = inject(Router)
+  const currentUser = inject(CurrentUserServiceService)
 
-  let authenticationStatus = false
-
-  if(authenticationStatus) return true
+  if(currentUser != null) return true
 
   router.navigate(["login"])
   return false
